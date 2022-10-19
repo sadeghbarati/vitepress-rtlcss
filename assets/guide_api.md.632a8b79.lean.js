65c58b9688e896736ac4874bcbebc550a2dda08e
Sadegh Barati <sadeghbaratiwork@gmail.com>
2022-10-19 10:22:10 -0700

    vitepress rtlcss

---
+++ assets/guide_api.md.632a8b79.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,a as e}from"./chunks/framework.ead4eca5.js";const d=JSON.parse('{"title":"API Reference","description":"","frontmatter":{},"headers":[],"relativePath":"guide/api.md","lastUpdated":1653495523000}'),o={name:"guide/api.md"},l=e("",26),p=[l];function t(c,r,F,y,D,i){return a(),n("div",null,p)}const A=s(o,[["render",t]]);export{d as __pageData,A as default};
